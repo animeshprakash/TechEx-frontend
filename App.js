5116dc8f6d2121e7d6e2591aac990a7ee40c8e45
Animesh Prkash <animeshprakash990@gmail.com>
2020-10-31 00:52:41 +0300

    Added gsap. To change: Entire Design :')

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+const timeline = gsap.timeline({defaults:{ease:"power1.out"}});
+timeline.fromTo("nav",{opacity: 0},{opacity: 1,duration: 1,})
+timeline.to("#scroll",{opacity:1, duration: 1,delay:.3},"-=1");
+timeline.fromTo("#logo",{opacity:0 ,y:"+3%"},{opacity:1,y:"0%",duration: 0.4},"-=1");
 new fullpage('#fullpage',{
     menu: '#menu',
     navigation: true,
@@ -6,4 +10,8 @@ new fullpage('#fullpage',{
     scrollHorizontally: true,
     paddingTop:'30px',
     parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+    onLeave: (origin,destination,direction)=>{
+        const startPoint = destination.item;
+        
+    }
 });
