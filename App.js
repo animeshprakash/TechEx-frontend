539b8ff609225737aec9f80795078d8ced1c5609
Animesh Prkash <animeshprakash990@gmail.com>
2020-10-30 17:41:21 +0300

    Added git to proj

---
+++ App.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage',{
+    menu: '#menu',
+    navigation: true,
+    navigationPosition: 'right',
+    autoScrolling:true,
+    scrollHorizontally: true,
+    paddingTop:'30px',
+    parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+});
