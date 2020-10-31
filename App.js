747b86936577ffa964fd4ef13ab4888542c60e35
Animesh Prkash <animeshprakash990@gmail.com>
2020-10-31 15:29:00 +0300

    Designed changed. Todo: Contact us page and img slider

--- App.js
+++ App.js
@@ -1,7 +1,3 @@
-const timeline = gsap.timeline({defaults:{ease:"power1.out"}});
-timeline.fromTo("nav",{opacity: 0},{opacity: 1,duration: 1,})
-timeline.to("#scroll",{opacity:1, duration: 1,delay:.3},"-=1");
-timeline.fromTo("#logo",{opacity:0 ,y:"+3%"},{opacity:1,y:"0%",duration: 0.4},"-=1");
 new fullpage('#fullpage',{
     menu: '#menu',
     navigation: true,
@@ -11,7 +7,24 @@ new fullpage('#fullpage',{
     paddingTop:'30px',
     parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
     onLeave: (origin,destination,direction)=>{
-        const startPoint = destination.item;
         
+        const back = gsap.timeline({defaults:{ease:"power4.out"}});
+        const startPoint = destination.item;
+        console.log(direction)
+        if(startPoint===noRotate || direction=='up'){
+            back.pause()
+        }else{
+            back.resume()
+            back.fromTo('.bg',{rotation: -5},{rotation: 1,duration: 1,delay: 0.3});
+            back.fromTo('.sectionContainer',{opacity:0, y:'-10%'},{opacity:1, y:'0',duration: 1},"-=0.8")
+            document.getElementById("phoneImg").focus = ()=>{
+                
+            }
+        }
     }
 });
+const timeline = gsap.timeline({defaults:{ease:"power1.out"}});
+const noRotate = document.querySelector("#s1.section");
+timeline.fromTo("nav",{opacity: 0},{opacity: 1,duration: 1.5,})
+    .to("#scroll",{opacity:1, duration: 1,delay:.3},"-=1")
+    .fromTo("#logo",{opacity:0 ,y:"+3%"},{opacity:1,y:"0%",duration: 0.4},"-=1.3");
